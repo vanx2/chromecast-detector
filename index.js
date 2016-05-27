c6060daa9b2fb668d69383c1106ff035d35fc986
v <vanxx2@gmail.com>
2016-05-27 16:45:22 +0900

    add names option that limit to detect when match chromecast name

--- index.js
+++ index.js
@@ -6,20 +6,18 @@ var detector = mdns();
 detector.casts = {};
 
 // start to detect
-detector.listen = function (ttl){ // todo: get argv as options include name
-  var self = this;
+detector.listen = function (options){
+  var _self = this;
 
   // when get response
-  self.on('response', function (response) {
+  _self.on('response', function (response) {
     var name = ip = id = port = '';
     response.answers.forEach(function (an) {
-//console.log(an);
       if (an.type === 'PTR' && an.name === '_googlecast._tcp.local'){
         name = an.data.replace(/\._googlecast\._tcp\.local$/, '');
       }
     });
     response.additionals.forEach(function (ad) {
-//console.log(ad);
       if        ( ad.type === 'TXT' && ad.name === name + '._googlecast._tcp.local' ) {
         id   = ad.data.replace(/^id=/, '');
       } else if ( ad.type === 'A'   && ad.name === name + '.local' ) {
@@ -28,31 +26,41 @@ detector.listen = function (ttl){ // todo: get argv as options include name
         port = ad.data.port;
       }
     });
-    if (name && ip && ( ! self.casts.hasOwnProperty(id) || 
-        self.casts[id].ip != ip || self.casts[id].name != name ) ) {
-      self.casts[id] = {name: name, ip: ip, port: port, id: id};
-      console.log("[DETECT]", self.casts[id] );
-      self.emit('detect', self.casts[id]);
+    if (name && ip && 
+         ( ! _self.casts.hasOwnProperty(id) ||  _self.casts[id].ip != ip || _self.casts[id].name != name ) &&
+         ( ! ( options && options.hasOwnProperty("names") && options.names.indexOf(name) < 0 ) ) ) {
+      _self.casts[id] = {name: name, ip: ip, port: port, id: id};
+      if ( ! module.parent) { console.log("[DETECT]", _self.casts[id] ); }
+      _self.emit('detect', _self.casts[id]);
     }
   });
 
-  // send query
-  self.query({
+  _self.sendQuery();
+
+  // die after 120sec that is chromecast mdns ttl
+  if ( options && options.hasOwnProperty("ttl") && isFinite(options.daemon.ttl) ) {
+    var timer = setTimeout(_self.terminate, ttl * 1000 );
+  }
+};
+
+detector.sendQuery = function (){
+console.log("Q");
+  this.query({
     questions:[{
       name: '_googlecast._tcp.local',
       type: 'ptr'
     }]
   });
-  // die after 120sec that is chromecast mdns ttl
-  if (ttl != true && ! ttl.match(/forever/i) && ! ttl.match(/daemon/i) ) {
-    var timer = setTimeout(self.destroy, ( (isNaN(ttl)) ? 120 : 0 + ttl ) * 1000 );
-  }
+  
+};
+detector.terminate = function (){
+  this.destroy();
 };
 
 if (module.parent) {
   module.exports = detector;
 } else {
-  detector.listen( ( process.argv.length > 2) ? process.argv.slice(2) : false );
+  detector.listen( ( process.argv.length > 2) ? JSON.parse( process.argv.slice(2) ) : undefined );
 }
 
 
